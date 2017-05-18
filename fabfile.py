fa70a4c8d290dfc3c43823b114775b96b9e90591
HIKARU <rosefanyx123@gmail.com>
2017-05-18 20:05:04 +0700

    update no_result ui pos & add keep db&gun_config in fabric

--- fabfile.py
+++ fabfile.py
@@ -7,6 +7,9 @@ from fabric.contrib.files import exists
 env.hosts=['hikaru@103.80.29.187']
 env.password="123123"
 
+
+KEEP_DB_AND_GUN_CONFIG = True
+
 def npm_build():
     local("npm run build")
 
@@ -16,7 +19,15 @@ def clean_build_useless_images():
 
 def zip():
     # local("mv build soul_manga")
-    local("zip -qr build.zip build server/web_server.py server/soul_manga.db server/gun_config.py")
+    # 默认只放入client/server code，因为vps上crontab在自己运行会更新db，用本地反而有问题,config 一般也不会改，我曹，不行
+    # todo 卧槽！！！这里有大问题，如果下面是用rm -rf soul_manga的话，这两个文件都得跪，要不先在vps rm之前先保存到~然后再移动回去？
+    # local("zip -qr build.zip build server/web_server.py server/soul_manga.db server/gun_config.py")
+
+    if KEEP_DB_AND_GUN_CONFIG:
+        local("zip -qr build.zip build server/web_server.py ")
+    else:
+        local("zip -qr build.zip build server/web_server.py server/soul_manga.db server/gun_config.py")
+
 
 def local_build():
     npm_build()
@@ -26,6 +37,8 @@ def local_build():
 def upload_to_remote():
     put("build.zip", use_sudo=True)
     # 为了mv 的非空目录....
+    if KEEP_DB_AND_GUN_CONFIG:
+        run("cp -f soul_manga/soul_manga.db soul_manga/gun_config.py ~/")
     run("rm -rf soul_manga")
     # run("mkdir soul_manga")
 
@@ -34,6 +47,9 @@ def upload_to_remote():
     run("mv build soul_manga")
     run("mv -f server/* soul_manga/")
     run("rm -rf server")
+
+    if KEEP_DB_AND_GUN_CONFIG:
+        run("mv soul_manga.db gun_config.py ~/soul_manga")
 
 def start_gunicorn():
     run("gun")
